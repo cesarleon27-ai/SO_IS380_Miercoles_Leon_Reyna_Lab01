d12264309d32de15675905de2531b182b61873a9
Leon Reyna Cesar Antonio <cesar.leon.27@unsch.edu.pe>
2026-09-13 21:06:14 -0500

    Implementacion de lectura del nombre del proceso

--- lab01.c
+++ lab01.c
@@ -13,6 +13,8 @@ int main() {
         printf("\n--- Proceso %d ---\n", i + 1);
         printf("Ingrese PID: ");
         scanf("%d", &procesos[i].pid);
+        printf("Ingrese nombre: ");
+        scanf("%s", procesos[i].nombre);
     }
     return 0;
 }
